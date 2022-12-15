4e9daf800819628f2e98d532b438b6d93deb6cc4
sworrrnimaS <077bct046.swornima@sagarmatha.edu.np>
2022-12-15 08:29:16 +0545

    Learned dictionary

---
+++ Classes/class_8_and_9/dictionary.py
@@ -0,0 +1,25 @@
+'''key value pair save garna use hunchha'''
+
+dictionary_1 = {'a':104, 4:'four', 'c':None}   #key:value ->key int,float,string j rakhna ni payo
+# print(dictionary_1)
+
+# print(dictionary_1['a'])                 #key diyo bhane tesko value print garchha
+
+# print (len(dictionary_1))                   #gives dictionary_1 length
+
+# print(dictionary_1.values())             #prints values of dictionary --> dict_values([104, 'four', None])
+# print(type(dictionary_1.values()))      #dictionary_1.values() class ho bhanera dekhauchha
+
+# print(dictionary_1.keys())                 #prints keys of dictionary -->dict_keys(['a', 4, 'c'])
+# print(type(dictionary_1.keys()))          #(dictionary_1.keys()) class ho bhanera dekhauchha 
+
+# for key,value in dictionary_1.items():    #keys ra values dekhaucha 
+#     print(key)
+#     print(value)
+#     print(key,value)
+
+# for key in dictionary_1.keys():
+#     print(key)
+
+# for value in dictionary_1.values():
+#     print(value)
