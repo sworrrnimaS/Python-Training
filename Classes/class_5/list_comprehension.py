9f6e183df4c853932c4260df5f7d4db8d2f540ed
sworrrnimaS <077bct046.swornima@sagarmatha.edu.np>
2022-12-15 07:54:41 +0545

    any modified files till class 8

--- Classes/class_5/list_comprehension.py
+++ Classes/class_5/list_comprehension.py
@@ -1,8 +1,8 @@
 #list comprehension is basically used for loops, it is advaced form of python
 
 
-my_es = "Hi and hello and bye hehe"
-enu_list = my_es.split(" ")
+# my_es = "Hi and hello and bye hehe"
+# enu_list = my_es.split(" ")
 # print(enu_list)
 # for index,item in enumerate(enu_list):
 #     print(index,item)
@@ -14,6 +14,23 @@ enu_list = my_es.split(" ")
 # print(enu_list)
 
 #Using list comprehension
-b = [item.upper() for item in enu_list if enu_list.index(item) % 2 == 0]
-#k garne ho,loop,condition
-print(b)
+# b = [item.upper() for item in enu_list if enu_list.index(item) % 2 == 0]
+# #k garne ho,loop,condition
+# print(b)
+
+
+#2-D matrix addition
+# Program to add two matrices using list comprehension
+
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+
+Y = [[5,8,1],
+    [6,7,3],
+    [4,5,9]]
+
+result = [[X[i][j] + Y[i][j]  for j in range(len(X[0]))] for i in range(len(X))]
+
+for r in result:
+   print(r)
